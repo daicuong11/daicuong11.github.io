465e7b8e447ebe6c9fb07c8fc75a0be8287ec30c
CuongLD6510 <130262308+daicuong11@users.noreply.github.com>
2026-04-08 21:12:11 +0700

    refactor: config https

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import basicSsl from "@vitejs/plugin-basic-ssl"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [inspectAttr(), react()],
+  plugins: [basicSsl(), inspectAttr(), react()],
+  server: {
+    https: {},
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
